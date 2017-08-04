b25017f8c47f6a64740622bec90298ee6d6a0a24
Ali Arsalan <scac_1041@hotmail.com>
2017-08-04 16:07:25 +0300

    Update chatbot.R

--- chatbot.R
+++ chatbot.R
@@ -2,15 +2,15 @@ library(plumber)
 library(httr)
 library(data.table)
 library(RCurl)
+library(jsonlite)
 
 
+#* @serializer html
 #* @get /
 myFunc <- function(hub.mode, hub.challenge, hub.verify_token){
 if(hub.verify_token == "RRocker"){
-	finalVal <- c(hub.challenge, "OK")
 	return(hub.challenge)
  } else{
 return(paste("Verification Token mismatch", 403))
  }
 }
-
